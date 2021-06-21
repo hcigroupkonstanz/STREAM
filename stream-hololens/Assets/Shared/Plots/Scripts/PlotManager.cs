3f37f308f8191295ca4398b164206b83a526665c
Sebastian Hubenschmid <Sebastian.Hubenschmid@uni-konstanz.de>
2021-06-21 18:18:46 +0200

    Cleanup

--- stream-hololens/Assets/Shared/Plots/Scripts/PlotManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Assets.Modules.Networking;
-
-namespace Assets.Modules.Plots
-{
-    public class PlotManager : Manager<Plot>
-    {
-        protected override int Channel => NetworkChannel.PLOT;
-    }
-}
--- stream-hololens/Assets/Shared/WebClients/Scripts/WebClientManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using Assets.Modules.Networking;
-
-namespace Assets.Modules.WebClients
-{
-    public class WebClientManager : Manager<WebClient>
-    {
-        protected override int Channel => NetworkChannel.WEBCLIENT;
-    }
-}
